52c60b4dc4cb4d1492ef137187670471fc8f72b8
Prachi Parekh <parekhprachi410@gmail.com>
2026-06-08 00:03:48 +0000

    final web deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL:
-    "https://scaling-space-halibut-7vjqqg967prqc4vg-5000.app.github.dev/api"
+    "https://prohands-backend.onrender.com/api"
 });
 
 api.interceptors.request.use((config) =>
